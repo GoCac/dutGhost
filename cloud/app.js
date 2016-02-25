fbe919e9a6486cf838680e92da04736448cb2337
宫 酉昊 <ezronne@icloud.com>
2016-02-25 14:48:44 +0800

    update

--- cloud/app.js
+++ cloud/app.js
@@ -12,5 +12,55 @@ app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
+app.get('/test', function(req, res) {
+	var CronJob = require('cron').CronJob;
+	var http = require('http');
+
+	var serialNumbers = new Array();
+	for (var i = 0;i < 100000;i++){
+	    serialNumbers[i] = 201200000 + i;
+	}
+
+	var Student = AV.Object.extend('student'); 
+
+	new CronJob('*/1 * * * * *',function(){
+	    var serialNumer = serialNumbers.shift();
+	    var queryString = "http://202.118.65.53/mobileplatform/unifiedEntranceModel.do?functionPyname=cxyktjbxx&param={%22ydusertype%22:%220%22,%22imei%22:%22%3C27dfe3be%202fcfd3d1%200718b0ee%20a7d21cea%20af88da96%2049624317%20bbe2e244%2094157914%3E%22,%22param%22:%22"+ serialNumer.toString() +"%22,%22appid%22:%22dlut-ylkxt%22}";
+	    var body = '';
+	    var req = http.get(queryString, function(res) {
+	        console.log("Got response: " + res.statusCode);
+	        res.on('data',function(d){
+	            body += d;
+	        }).on('end', function(){
+	            var obj = eval("("+body+")");
+	            if (obj.idserial.toString().length == 9){
+	            	console.log(obj.username);
+					var query = new AV.Query(Student);  
+       				query.equalTo("serialNumber", Number(obj.idserial));
+       				query.find().then(function(result) {
+						// 成功获得实例
+					  	console.log('重复的');  
+						if (result.get('serialNumber') == Number(obj.idserial)) {
+					  		console.log('重复的');  
+						}else{
+							console.log('初次添加');       
+							var student = new Student(); 
+							student.set("name",obj.username);
+							student.set("serialNumber",Number(obj.idserial));
+							student.save();  
+						}
+					}, function(error) {
+						// 失败了
+					});
+	            }
+	        });
+	    }).on('error', function(e) {
+	        console.log("Got error: " + e.message);
+	    });
+	    req.end();
+
+	},null,true,null);
+});
+
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
