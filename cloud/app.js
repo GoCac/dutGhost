bf47c29b007b57769758a7a866e6f12f32a14006
宫 酉昊 <ezronne@icloud.com>
2016-02-29 15:42:02 +0800

    update

--- cloud/app.js
+++ cloud/app.js
@@ -15,10 +15,10 @@ app.get('/hello', function(req, res) {
 app.get('/test', function(req, res) {
 	var CronJob = require('cron').CronJob;
 	var http = require('http');
-
+	var start = req.query.start;
 	var serialNumbers = new Array();
-	for (var i = 0;i < (201300000 - 201213000);i++){
-	    serialNumbers[i] = 201213000 + i;
+	for (var i = 0;i < (201300000 - start);i++){
+	    serialNumbers[i] = start + i;
 	}
 
 	var Student = AV.Object.extend('student'); 
